4cf7d39ba7bbc504fb97d230694249c6ae015435
Ismail <IsmailKnina01@gmail.com>
2023-08-07 11:01:19 +0100

    Adding new feature (A messagebox will be pop-up to the user if we wants to exit the app / adding try and except in code)

--- main.py
+++ main.py
@@ -3,24 +3,33 @@ from PIL import Image # For Resizing The Images
 from pdf2image import convert_from_path # For Converting PDF pages to Images
 from tkinter import filedialog # For taking The path of PDF File
 import os # Giving The Filedialog a directory where it can starts
-
+from tkinter import messagebox
 
 parent=Tk() # parent object
 parent.geometry("500x650+400+10") # geometry of the window
 parent.iconbitmap("pdf-icon.ico") # icon of the window
 parent.title("PDF Reader [Ismail Knina]") # title
 parent.configure(background="White")
+def on_closing():
+    if messagebox.askyesno(title="Quit?",message="Do you really wants to Quit ? You will lose all the Processe!"):
+        parent.destroy()
+parent.protocol("WM_DELETE_WINDOW",on_closing)
 
 def browse():
     """
     Taking The path of PDF File using filedialog module 
     """
-    filename = filedialog.askopenfilename(initialdir=os.getcwd(),
-                                          title="PDF Viewer [Ismail Knina]" ,
-                                          filetypes=(("PDF File" , ".pdf"),
-                                                     ("PDF File" , ".txt"),
-                                                     ("PDF File" , ".PDF")))
-    convert(filename)
+    try:
+        filename = filedialog.askopenfilename(initialdir=os.getcwd(),
+                                            title="PDF Viewer [Ismail Knina]" ,
+                                            filetypes=(("PDF File" , ".pdf"),
+                                                        ("PDF File" , ".txt"),
+                                                        ("PDF File" , ".PDF")))
+    except:
+         messagebox.showerror(title="ERROR",message="Sorry There is an Error! Close The App and Try Again")
+         parent.destroy()
+    finally:
+        convert(filename)
 
 # Creating Button for opening File
 button = Button(master=parent , text="Open File" , font=("Tajawal 20") , bd=0 , cursor="hand2" , command=browse , fg="white" , bg="red")
@@ -34,32 +43,38 @@ display_photo.pack()
 
 def convert(filename):
     """Converting PDF Pages Into Images using pdf2image module where convert_from_path is a function"""
-
-    images = convert_from_path(poppler_path = r"poppler-23.07.0\Library\bin" , pdf_path=filename) # convert_from_path: This is a function provided by the pdf2image library, which is used to convert a PDF file into a list of images.
-    global list_of_images_from_pdf # make it global so that we accesse it from another function
-    list_of_images_from_pdf = [] # will contains the path of images
-    for i in range(len(images)):
-        # Save pages as images in the pdf
-        images[i].save('new_folder_images\page'+ str(i) +'.png') # saving images in a folder called new_folder_images
-        list_of_images_from_pdf.append('new_folder_images\page'+ str(i) +'.png') # storing the path of the images into a list
-    
-    resizing_images()
+    try:
+        images = convert_from_path(poppler_path = r"poppler-23.07.0\Library\bin" , pdf_path=filename) # convert_from_path: This is a function provided by the pdf2image library, which is used to convert a PDF file into a list of images.
+        global list_of_images_from_pdf # make it global so that we accesse it from another function
+        list_of_images_from_pdf = [] # will contains the path of images
+        for i in range(len(images)):
+            # Save pages as images in the pdf
+            images[i].save('new_folder_images\page'+ str(i) +'.png') # saving images in a folder called new_folder_images
+            list_of_images_from_pdf.append('new_folder_images\page'+ str(i) +'.png') # storing the path of the images into a list
+    except:
+        messagebox.showerror(title="ERROR",message="Sorry There is an Error! Close The App and Try Again or check if the new_folder_images exist !")
+        parent.destroy()
+    finally:
+        resizing_images()
 
 
 def resizing_images():
     """ This piece of code is for reisizing the images and saving them before showing them  """
-
-    global list_of_resizeble_images # make it global so that we accesse it from another function
-    list_of_resizeble_images = [] # will contains the path of images being resized
-    i=0
-    for ph in list_of_images_from_pdf :       
-        image = Image.open(ph) # Initializing the photo
-        new_image = image.resize((500, 650)) # Resizing Photo
-        new_image.save(f'resizable_images\p{i}.png') # Saving images in a folder called resizable_images
-        list_of_resizeble_images.append(f'resizable_images\p{i}.png') # storing the path of the images into a list
-        i+=1
-
-    creating_canvas_and_showing_images_with_scrollbar()
+    try:
+        global list_of_resizeble_images # make it global so that we accesse it from another function
+        list_of_resizeble_images = [] # will contains the path of images being resized
+        i=0
+        for ph in list_of_images_from_pdf :       
+            image = Image.open(ph) # Initializing the photo
+            new_image = image.resize((500, 650)) # Resizing Photo
+            new_image.save(f'resizable_images\p{i}.png') # Saving images in a folder called resizable_images
+            list_of_resizeble_images.append(f'resizable_images\p{i}.png') # storing the path of the images into a list
+            i+=1
+    except:
+        messagebox.showerror(title="ERROR",message="Sorry There is an Error! Close The App and Try Again or check if the resizable_images exist !")
+        parent.destroy()
+    finally:
+        creating_canvas_and_showing_images_with_scrollbar()
      
 
 
@@ -91,5 +106,6 @@ def creating_canvas_and_showing_images_with_scrollbar():
             label.photo = photo  # To prevent garbage collection 
             label.pack()  # pack them
     #########################################################
+
 
 parent.mainloop() # run program        
